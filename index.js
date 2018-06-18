08efd8bd174174e7d62275c8e5b08b465bc5dc9c
Chase Davis <cdavis@cdaviss-MacBook-Pro.local>
2018-06-18 08:48:46 -0700

    manual setup of cluster mode

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
+process.env.UV_THREADPOOL_SIZE = 1; // every child only has 1 thread
+
 const cluster = require('cluster');
-const express = require('express');
-const app = express();
 
 if (cluster.isMaster) {
-
+    
     // forks can run on same port and allow for multiple threads of our event loop!
-    for(let i = 0; i < 100; i++){
+    // for(let i = 0; i < 100; i++){
         // this is NOT performent
-        cluster.fork();
+        // cluster.fork(); // every child has it's own seperate threadpool
         // in terminal run:
-            // ab -c 50 -n 500 localhost:3000/fast
-    }
-    
-
-    // master can NOT run on same port 
-    // app.listen(3000); * WILL THROW ERROR EADDRINUSE null:3000 *
-} else {
-    
-    function doWork(duration) {
-        const start = Date.now();
-        while(Date.now() - start < duration) {};
-    }
-    
-    app.get('/', (req,res) => {
-        doWork(10000); 
-        // because event loop is single threaded,
-        // doWork will delay everything else for ~ 5 seconds
-        // node server can do absolutely nothing ( including handle other incomming requests )
-        // during this time
-        res.send('slow');
+        // ab -c 50 -n 500 localhost:3000/fast
+        // }
+        
+        // best to match number of clusters with number of physical CPU cores running on machine
+        cluster.fork();
+        cluster.fork();
+        
+        
+        // master can NOT run on same port 
+        // app.listen(3000); * WILL THROW ERROR EADDRINUSE null:3000 *
+    } else {
+        const crypto = require('crypto');
+        const express = require('express');
+        const app = express();
+        
+        app.get('/', (req,res) => {
+        crypto.pbkdf2('a','b', 100000, 512, 'sha512', () => {
+            res.send('slow');
+        });
     })
 
     app.get('/fast', (req,res) => {
